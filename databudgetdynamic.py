d53d3b2be58e1f6e16a392dbdd01b6d52a895d57
Wyatt Robinson <wer1928@rit.edu>
2026-02-01 20:30:05 -0500

    events now per each medium, general cleanup

--- databudgetdynamic.py
+++ databudgetdynamic.py
@@ -33,6 +33,8 @@ class Medium:
     overhead : float
     sources : tuple
     color : str
+    events : list[tuple[str,float]]
+    max_rate : float
 
 media : list[Medium] = []
 
@@ -41,14 +43,18 @@ if media_count == -1:
     print('DEBUG MODE') # for less tedious testing
     med_color_ind = random.randint(0, len(med_colors)-1)
     media = [
-        Medium(name='First Med', total=13.24, overhead=1.1, color=med_colors[med_color_ind], sources=(
-            Source(type='Video', size=2),
-            Source(type='Sensor Data', size=7)
+        Medium(name='First Med', total=13.24, overhead=1.1, color=med_colors[med_color_ind],
+            events=[('Arm', 0), ("Boost", 1), ("Later", 0)], max_rate=1,
+            sources=(
+                Source(type='Video', size=2),
+                Source(type='Sensor Data', size=7),
         )),
-        Medium(name='Second Medium', total=128.9, overhead=2.3, color=med_colors[(med_color_ind+1)%len(med_colors)], sources=(
-            Source(type='Video', size=35.17),
-            Source(type='Sensor Data', size=15),
-            Source(type='Logs', size=2),
+        Medium(name='Second Medium', total=128.9, overhead=2.3, color=med_colors[(med_color_ind+1)%len(med_colors)],
+            events=[('Arm', 0), ("Boost", 1), ("Later", 2)], max_rate=2,
+            sources=(
+                Source(type='Video', size=35.17),
+                Source(type='Sensor Data', size=15),
+                Source(type='Logs', size=2),
         ))
     ]
     media_count = 0 # to skip media input loop
@@ -83,38 +89,42 @@ for n in range(media_count): # get storage media information
 
     this_color = med_colors[n % len(med_colors)] # color for this source
     legend_handles.append(patches.Patch(color=this_color, label=name))
-    media.append(Medium(name, total_storage, over_head, tuple(sources), this_color))
 
-# get events
-events : list[tuple[str,dict[Medium,float]]] = []
-event_count = int(input('Number of events: '))
-max_rate = -1
+    # get events for this medium
+    events : list[tuple[str,float]] = []
+    event_count = int(input(f'\tNumber of events for medium {n+1}: '))
+    max_rate = -1
 
-for n in range(0, event_count):
-    name = input(f'\tEvent {n+1} name: ')
-    rates : dict[Medium,float] = {}
-    for med in media:
-        rate = float(input(f'\t\t{med.name} (MB/s): '))
-        rates[med] = rate
+    for e in range(0, event_count):
+        name = input(f'\t\tEvent {n+1} name: ')
+        rate = float(input(f'\t\t{name} rate (MB/s): '))
         max_rate = max(max_rate, rate)
-    events.append((name, rates))
+        events.append((name, rate))
 
+    media.append(Medium(name, total_storage, over_head, tuple(sources), this_color, events, max_rate))
+
+# set up figure
 fig : matplotlib.figure.Figure = plt.figure()
+fig.set_figwidth(4.8*(len(media)+0.5)) # additional half for legend
+# print(fig.get_figheight())
 subfigs : list[matplotlib.figure.SubFigure] = fig.subfigures(
-    ncols=len(media)+1
-    #wspace=,
-    #layout='constrained'
+    ncols=len(media)+1,
+    width_ratios=([1] + [2]*len(media)),
+    wspace=1
 )
 
 # drawing time
 for n in range(len(media)):
-    current_axes = subfigs[n].subplots(
+    current_axes = subfigs[n+1].subplots( # n+1 to leave space for legend
         nrows=2,
         ncols=1,
+        height_ratios=[2,1],
         #layout='constrained'
     )
     usage_ax : matplotlib.axes.Axes =  current_axes[0]
-    med : Medium = media[n] 
+    event_ax : plt.Axes = current_axes[1]
+    med : Medium = media[n]
+    subfigs[n+1].suptitle(med.name, ha='left', x=0.1) 
 
     used = 0
     for s in med.sources:
@@ -154,32 +164,28 @@ for n in range(len(media)):
     zoom2 = patches.ConnectionPatch(xyA=(med.overhead+used, 1-OVERVIEW_HEIGHT/2), xyB=(med.total, ZOOM_HEIGHT/2), coordsA='data', color=med.color)
     usage_ax.add_artist(zoom2)
 
-    # title it
-    usage_ax.annotate(med.name, xy=(-0.1,0.5), xycoords='axes fraction', rotation=90, ha='center', va='center', size='large')
+    # plot event rates for this medium
+    events = med.events
+    max_rate = med.max_rate
+    x_labels = [ev[0] for ev in med.events]
 
-# plot event rates
-event_ax : plt.Axes = subfigs[len(media)].add_axes([0.1,0.1,0.8,0.8])
-x_labels = [ev[0] for ev in events]
-y_labels = reversed([med.name for med in media])
-for n in range(len(events)):
-    ev_name, rates = events[n]
-    for i in range(len(media)):
-        rate = rates[media[i]]
-        event_ax.barh([len(media)-i-1], width=1, height=ZOOM_HEIGHT*(rate/max_rate), left=n, color=media[i].color, align='edge')
-event_ax.set_xticks(np.arange(len(events)), x_labels)
-event_ax.set_yticks(np.arange(len(media)), y_labels)
-event_ax.spines[['right','top']].set_visible(False)
-event_ax.set_xlim(right=len(events)+0.2)
-event_ax.set_ylim(top=len(media)+1)
-arr = patches.FancyArrowPatch((len(events)+0.1, 0), (len(events)+0.1, ZOOM_HEIGHT),
-                            arrowstyle='|-|,widthA=0.25,widthB=0.25', mutation_scale=20)
-event_ax.add_patch(arr)
-event_ax.annotate(f"{max_rate} MB/S", (.7, .5), xycoords=arr, ha='left', va='center')
+    for n in range(len(events)): # draw rates
+        ev_name, rate = events[n]
+        event_ax.barh(0, width=1, height=ZOOM_HEIGHT*(rate/max_rate), left=n, color=med.color, align='edge')
+
+    event_ax.set_xticks(np.arange(len(events)), x_labels)
+    event_ax.set_yticks([]) # disable y-axis ticks
+    event_ax.spines[['right','top']].set_visible(False)
+    event_ax.set_xlim(right=len(events)+0.2)
+    event_ax.set_ylim(top=1)
+    arr = patches.FancyArrowPatch((len(events)+0.1, 0), (len(events)+0.1, ZOOM_HEIGHT),
+                                arrowstyle='|-|,widthA=0.25,widthB=0.25', mutation_scale=20)
+    event_ax.add_patch(arr)
+    event_ax.annotate(f"{max_rate} MB/S", (.7, .5), xycoords=arr, ha='left', va='center')
 
 # create legend
 legend_handles.append(patches.Patch(color=oh_color, label='Filesystem\nOverhead'))
 legend_handles.append(patches.Patch(color=used_color, label='Used'))
-fig.legend(handles=legend_handles,loc='upper left', bbox_to_anchor=(0,0.9))
-# fig.subplots_adjust(left=0.35)
+subfigs[0].legend(handles=legend_handles,loc='center right')
 
 plt.show()
